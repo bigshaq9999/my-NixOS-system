68533ac80df3259ceff899bfd4cf9b8b10008190
bigshaq9999 <97749920+bigshaq9999@users.noreply.github.com>
2024-04-28 01:07:24 +0700

    format: vscode settings

--- home-manager/programs/vscode/settings.nix
+++ home-manager/programs/vscode/settings.nix
@@ -4,9 +4,10 @@ _: {
     "zenMode.hideLineNumbers" = false;
 
     "editor.fontSize" = 16;
+    "editor.fontFamily" = "'FiraCode Nerd Font Mono'";
+    "editor.fontLigatures" = true;
     "editor.cursorSurroundingLines" = 6;
-    "editor.fontFamily" = "'Comic Mono', 'monospace', monospace";
-    "editor.rulers" = [80];
+    "editor.rulers" = [ 80 ];
     "editor.minimap.enabled" = false;
     "editor.mouseWheelZoom" = true;
     "editor.guides.bracketPairs" = "active";
@@ -16,7 +17,7 @@ _: {
 
     "debug.console.fontSize" = 16;
     "security.workspace.trust.untrustedFiles" = "open";
-    "terminal.integrated.fontFamily" = "Hack";
+    "terminal.integrated.fontFamily" = "Firacode Nerd Font Mono Med";
     "window.menuBarVisibility" = "toggle";
     "window.zoomLevel" = 0.75;
     "workbench.sideBar.location" = "right";
@@ -33,13 +34,9 @@ _: {
 
     "codium.enabled" = true;
 
-    "[python]" = {
-      "editor.formatOnType" = true;
-    };
+    "[python]" = { "editor.formatOnType" = true; };
 
-    "[html]" = {
-      "editor.defaultFormatter" = "vscode.html-language-features";
-    };
+    "[html]" = { "editor.defaultFormatter" = "vscode.html-language-features"; };
 
     "[nix]" = {
       "editor.defaultFormatter" = "brettm12345.nixfmt-vscode";
