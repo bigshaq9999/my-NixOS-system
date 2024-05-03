b18178b3108ba6afa88e0c761cc7e73d09985c36
bigshaq9999 <97749920+bigshaq9999@users.noreply.github.com>
2024-05-03 21:37:28 +0700

    fix(configuration.nix): xserver.libinput -> libinput

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -121,7 +121,7 @@
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
+  services.libinput.enable = true;
 
   users.users = {
     kunny = {
