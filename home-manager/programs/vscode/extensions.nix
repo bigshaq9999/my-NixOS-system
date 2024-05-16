98f2358a8311112cbc13b8b4b2a5d6708e34c4c3
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-05-16 19:37:47 +0300

    chore(vscode/extensions): re-organize extensions
    - Sorted alphabetically while accounting for importance and category

--- home-manager/programs/vscode/extensions.nix
+++ home-manager/programs/vscode/extensions.nix
@@ -11,43 +11,61 @@ let
 in
 {
   programs.vscode.extensions = builtins.attrValues {
+    # General Development
     vscode-remote-extensionpack = vscode-marketplace.ms-vscode-remote.vscode-remote-extensionpack;
-    vscode-icons = vscode-marketplace.vscode-icons-team.vscode-icons;
     vsliveshare = vscode-marketplace.ms-vsliveshare.vsliveshare;
     vscodeintellicode = vscode-marketplace.visualstudioexptteam.vscodeintellicode;
+    copilot = vscode-marketplace-release.github.copilot;
+    copilot-chat = vscode-marketplace-release.github.copilot-chat;
 
+    # Language Specific
+    # Bash
     bash-ide-vscode = vscode-marketplace.mads-hartmann.bash-ide-vscode;
 
+    # Javascript/Typescript
+    vscode-language-babel = vscode-marketplace.mgmcdermott.vscode-language-babel;
+    steoates-autoimport = vscode-marketplace.steoates.autoimport;
+    vscode-eslint = vscode-marketplace.dbaeumer.vscode-eslint;
+    vscode-typescript-next = vscode-marketplace.ms-vscode.vscode-typescript-next;
+
+    # Nix
     bbenoist-nix = vscode-marketplace.bbenoist.nix;
     nix-ide = vscode-marketplace.jnoortheen.nix-ide;
     brettm12345-nixfmt-vscode = vscode-marketplace.brettm12345.nixfmt-vscode;
 
-    direnv = vscode-marketplace.mkhl.direnv;
+    # Python
     python = vscode-marketplace.ms-python.python;
     vscode-pylance = vscode-marketplace.ms-python.vscode-pylance;
     jupyter = vscode-marketplace.ms-toolsai.jupyter;
-    vscode-typescript-next = vscode-marketplace.ms-vscode.vscode-typescript-next;
+    direnv = vscode-marketplace.mkhl.direnv;
 
-    vscode-language-babel = vscode-marketplace.mgmcdermott.vscode-language-babel;
-    steoates-autoimport = vscode-marketplace.steoates.autoimport;
-    vscode-eslint = vscode-marketplace.dbaeumer.vscode-eslint;
-
-    vscode-markdownlint = vscode-marketplace.davidanson.vscode-markdownlint;
-    latex-workshop = vscode-marketplace.james-yu.latex-workshop;
-    vscode-xml = vscode-marketplace.redhat.vscode-xml;
-    jsoncrack-vscode = vscode-marketplace.aykutsarac.jsoncrack-vscode;
-    code-spell-checker = vscode-marketplace.streetsidesoftware.code-spell-checker;
-
+    # C/C++
     vscode-clangd = vscode-marketplace.llvm-vs-code-extensions.vscode-clangd;
 
-    vim = vscode-marketplace.vscodevim.vim;
+    # LaTeX
+    latex-workshop = vscode-marketplace.james-yu.latex-workshop;
+
+    # XML
+    vscode-xml = vscode-marketplace.redhat.vscode-xml;
+
+    # JSON
+    jsoncrack-vscode = vscode-marketplace.aykutsarac.jsoncrack-vscode;
+
+    # Markup and Documentation
+    vscode-markdownlint = vscode-marketplace.davidanson.vscode-markdownlint;
+    code-spell-checker = vscode-marketplace.streetsidesoftware.code-spell-checker;
+
+    # UI Enhancement
+    vscode-icons = vscode-marketplace.vscode-icons-team.vscode-icons;
+    gruvbox = vscode-marketplace.jdinhlife.gruvbox;
     indent-rainbow = vscode-marketplace.oderwat.indent-rainbow;
+    errorlens = vscode-marketplace.usernamehw.errorlens;
+
+    # Code Formatting and Diff
     prettier-vscode = vscode-marketplace.esbenp.prettier-vscode;
     partial-diff = vscode-marketplace.ryu1kn.partial-diff;
-    errorlens = vscode-marketplace.usernamehw.errorlens;
-    gruvbox = vscode-marketplace.jdinhlife.gruvbox;
 
-    copilot = vscode-marketplace-release.github.copilot;
-    copilot-chat = vscode-marketplace-release.github.copilot-chat;
+    # Editor Enhancements
+    vim = vscode-marketplace.vscodevim.vim;
   };
 }
