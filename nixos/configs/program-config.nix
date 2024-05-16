960b125518e4ec20f964e2aaba91357e40f4cc87
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-05-16 20:00:56 +0300

    refactor(program-config): grouped pkgs under programs

--- nixos/configs/program-config.nix
+++ nixos/configs/program-config.nix
@@ -1,11 +1,9 @@
 { pkgs, ... }:
 {
-  programs.htop.enable = true;
-  programs.nano.syntaxHighlight = true;
-
-  # zsh
-  programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
-
-  programs.dconf.enable = true;
+  programs = {
+    nano.syntaxHighlight = true;
+    zsh.enable = true;
+    dconf.enable = true;
+  };
 }
