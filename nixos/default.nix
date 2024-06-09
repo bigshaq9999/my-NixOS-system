3e6e8978e330974e346e273e13d091cf07848c72
bigshaq9999 <97749920+bigshaq9999@users.noreply.github.com>
2024-06-09 19:59:34 +0700

    fix(home-manager): import home-manager as a module

--- nixos/default.nix
+++ nixos/default.nix
@@ -17,33 +17,22 @@ let
 
     nur.nixosModules.nur
 
-    { nixpkgs.overlays = [ nur.overlay ]; }
-    (
-      { pkgs, ... }:
-      let
-        nur-no-pkgs = import nur { nurpkgs = import nixpkgs { system = "x86_64-linux"; }; };
-      in
-      {
-        imports = [ nur-no-pkgs.repos.iopq.modules.xraya ];
-        services.xraya.enable = true;
-      }
-    )
-
     home-manager.nixosModules.home-manager
     {
       home-manager = {
         useGlobalPkgs = true;
         useUserPackages = true;
         users.nanachi = import ../home-manager/home.nix;
+        backupFileExtension = "backup";
         extraSpecialArgs = {
-          inherit inputs outputs system;
+          inherit inputs outputs;
         };
       };
     }
   ];
 in
 {
-  nanachi = inputs.nixpkgs.lib.nixosSystem {
+  nixos = inputs.nixpkgs.lib.nixosSystem {
     inherit system;
     inherit specialArgs;
     inherit modules;
