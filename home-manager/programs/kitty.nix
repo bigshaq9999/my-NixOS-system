892ea636f861c83b507a62050b381836f35937ed
bigshaq9999 <97749920+bigshaq9999@users.noreply.github.com>
2024-09-12 22:07:12 +0700

    home-manager/programs/kitty.nix: enable kitty

--- home-manager/programs/kitty.nix
+++ home-manager/programs/kitty.nix
@@ -4,9 +4,13 @@
     enable = true;
     font = {
       name = "FiraCode Nerd Font Mono";
-      size = "12"; 
-      theme = "Solarized Light";
+      size = 12; 
     };
+    theme = "Solarized Light";
     shellIntegration.enableZshIntegration = true;
+    settings = {
+      draw_minimal_borders = "yes";
+      hide_window_decorations = "no";
+    };
   };
 }
