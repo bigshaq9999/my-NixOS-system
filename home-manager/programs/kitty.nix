5a064749534c397c64c868e03080ea72851a2c34
bigshaq9999 <97749920+bigshaq9999@users.noreply.github.com>
2024-09-21 22:59:04 +0700

    hm/programs/kitty: init kitty

--- home-manager/programs/kitty.nix
+++ home-manager/programs/kitty.nix
@@ -1,16 +1,19 @@
-{ pkgs, ... }:
-{
+_: {
   programs.kitty = {
     enable = true;
+    shellIntegration.enableZshIntegration = true;
     font = {
       name = "FiraCode Nerd Font Mono";
-      size = 12; 
+      size = 12;
     };
-    theme = "Solarized Light";
-    shellIntegration.enableZshIntegration = true;
+    theme = "Solarized Dark - Patched";
     settings = {
       draw_minimal_borders = "yes";
+
       hide_window_decorations = "no";
+    };
+    keybindings = {
+        "ctrl+b" = "select_tab";
     };
   };
 }
