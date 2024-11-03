008dfd70ffaa3c1688fc268c0b5b9e68f8a007ff
bigshaq9999 <97749920+bigshaq9999@users.noreply.github.com>
2024-11-03 13:40:10 +0700

    nixos/configs/plasma: plasma5 -> plasma6

--- nixos/configs/plasma.nix
+++ nixos/configs/plasma.nix
@@ -1,10 +1,7 @@
 _: {
   services = {
-    displayManager.defaultSession = "plasmawayland";
     displayManager.sddm.enable = true;
-    xserver = {
-      enable = true;
-      desktopManager.plasma5.enable = true;
-    };
+    displayManager.sddm.wayland.enable = true;
+    desktopManager.plasma6.enable = true;
   };
 }
