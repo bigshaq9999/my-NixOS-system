19854e4707df79cb1dd207cd16581913e44a0822
bigshaq9999 <97749920+bigshaq9999@users.noreply.github.com>
2024-11-05 09:46:29 +0700

    hm/home: enable emacs

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -4,6 +4,10 @@ _: {
     gh.enable = true;
   };
 
+  programs.emacs = {
+    enable = true;
+  };
+
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
 }
