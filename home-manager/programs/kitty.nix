0262e792c7ace73cdff48276b10d17024c22c1bc
bigshaq9999 <97749920+bigshaq9999@users.noreply.github.com>
2025-03-01 10:32:00 +0700

    hm/programs/kitty:   theme: ayu-light -> Catppuccin-Latte   keybinds: {f1: visual mode on buffer}

--- home-manager/programs/kitty.nix
+++ home-manager/programs/kitty.nix
@@ -2,15 +2,20 @@ _: {
   programs.kitty = {
     enable = true;
     shellIntegration.enableZshIntegration = true;
+
+    themeFile = "Catppuccin-Latte";
     font = {
       name = "JetBrainsMonoNL Nerd Font Mono";
       size = 12;
     };
-    themeFile = "ayu_light";
+
     settings = {
       draw_minimal_borders = "yes";
-
       hide_window_decorations = "no";
+    };
+    
+    keybindings = {
+      "f1" = "launch --stdin-source=@screen_scrollback --type=tab vi";
     };
   };
 }
