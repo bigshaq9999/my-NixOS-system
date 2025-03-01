93b293ba034b836960fcd4b403d826c93d651f76
bigshaq9999 <97749920+bigshaq9999@users.noreply.github.com>
2025-03-01 10:28:17 +0700

    nixos/configs/plasma: exclude discover, orca

--- nixos/configs/plasma.nix
+++ nixos/configs/plasma.nix
@@ -1,7 +1,12 @@
-_: {
+{ pkgs }: {
   services = {
     displayManager.sddm.enable = true;
     displayManager.sddm.wayland.enable = true;
     desktopManager.plasma6.enable = true;
+  };
+
+  environment.plasma6.excludePackages = builtins.attrValues {
+    inherit (pkgs.kdePackages) discover;
+    inherit (pkgs) orca;
   };
 }
