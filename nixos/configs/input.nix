beac03c3fdd3d1ee15ecd703e87d6b3ca30a59ad
bigshaq9999 <97749920+bigshaq9999@users.noreply.github.com>
2025-03-29 00:38:44 +0700

    nixos/configs/input: make fcitx work better on niri

--- nixos/configs/input.nix
+++ nixos/configs/input.nix
@@ -1,17 +1,17 @@
 { pkgs, ... }:
 {
-  environment.sessionVariables = {
-    GTK_IM_MODULE = "fcitx";
-    QT_IM_MODULE = "fcitx";
-    XMODIFIERS = "@im=fcitx";
-    MOZ_USE_XINPUT2 = "1"; # For firefox, see wiki/Firefox#Use_xinput2
-  };
-
   i18n = {
     inputMethod = {
       enable = true;
       type = "fcitx5";
-      fcitx5.addons = builtins.attrValues { inherit (pkgs) fcitx5-mozc fcitx5-unikey; };
+      fcitx5.waylandFrontend = true;
+      fcitx5.addons = builtins.attrValues {
+        inherit (pkgs)
+          fcitx5-mozc
+          fcitx5-unikey
+          fcitx5-gtk
+          ;
+      };
     };
   };
 }
