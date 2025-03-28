0d77537504fed44a84e807f6a4d5047f79930ee0
bigshaq9999 <97749920+bigshaq9999@users.noreply.github.com>
2025-03-29 00:19:12 +0700

    hm/prgrams/kitty: catppucin-{latte->frappe}, more configs

--- home-manager/programs/kitty.nix
+++ home-manager/programs/kitty.nix
@@ -3,17 +3,22 @@ _: {
     enable = true;
     shellIntegration.enableZshIntegration = true;
 
-    themeFile = "Catppuccin-Latte";
+    themeFile = "Catppuccin-Frappe";
     font = {
       name = "JetBrainsMonoNL Nerd Font Mono";
       size = 12;
     };
 
     settings = {
-      draw_minimal_borders = "yes";
-      hide_window_decorations = "no";
+      draw_minimal_borders = false;
+      hide_window_decorations = true;
+      window_border_width = "0px";
+      tab_bar_edge = "top";
+      tab_bar_margin_width = "0.0";
+      tab_bar_style = "fade";
+      placement_strategy = "top-left";
     };
-    
+
     keybindings = {
       "f1" = "launch --stdin-source=@screen_scrollback --type=tab vi";
     };
